9fa779bd46e8ea8a9b3bdb599dbae0ca9fe75887
Andrej Tokarčík <andrej@goteleport.com>
2021-07-12 17:03:53 +0200

    Fix utmp integration test

--- integration/utmp_integration_test.go
+++ integration/utmp_integration_test.go
@@ -23,6 +23,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/api/constants"
 	apidefaults "github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
@@ -66,8 +67,9 @@ func TestRootUTMPEntryExists(t *testing.T) {
 		t.Skip("This test will be skipped because tests are not being run as root.")
 	}
 
-	s := newSrvCtx(t)
-	up, err := newUpack(s, teleportTestUser, []string{teleportTestUser}, wildcardAllow)
+	ctx := context.Background()
+	s := newSrvCtx(ctx, t)
+	up, err := newUpack(ctx, s, teleportTestUser, []string{teleportTestUser}, wildcardAllow)
 	require.NoError(t, err)
 
 	sshConfig := &ssh.ClientConfig{
@@ -148,7 +150,7 @@ func TouchFile(name string) error {
 }
 
 // This returns the utmp path.
-func newSrvCtx(t *testing.T) *SrvCtx {
+func newSrvCtx(ctx context.Context, t *testing.T) *SrvCtx {
 	s := &SrvCtx{}
 
 	t.Cleanup(func() {
@@ -156,7 +158,7 @@ func newSrvCtx(t *testing.T) *SrvCtx {
 			require.NoError(t, s.srv.Close())
 		}
 		if s.server != nil {
-			require.NoError(t, s.server.Shutdown(context.Background()))
+			require.NoError(t, s.server.Shutdown(ctx))
 		}
 	})
 
@@ -202,15 +204,13 @@ func newSrvCtx(t *testing.T) *SrvCtx {
 	require.NoError(t, err)
 	s.utmpPath = utmpPath
 
-	lockWatcher, err := services.NewLockWatcher(process.ExitContext(), services.LockWatcherConfig{
+	lockWatcher, err := services.NewLockWatcher(ctx, services.LockWatcherConfig{
 		ResourceWatcherConfig: services.ResourceWatcherConfig{
 			Component: teleport.ComponentNode,
 			Client:    s.nodeClient,
 		},
 	})
-	if err != nil {
-		return trace.Wrap(err)
-	}
+	require.NoError(t, err)
 
 	nodeDir := t.TempDir()
 	srv, err := regular.New(
@@ -247,8 +247,7 @@ func newSrvCtx(t *testing.T) *SrvCtx {
 	return s
 }
 
-func newUpack(s *SrvCtx, username string, allowedLogins []string, allowedLabels types.Labels) (*upack, error) {
-	ctx := context.Background()
+func newUpack(ctx context.Context, s *SrvCtx, username string, allowedLogins []string, allowedLabels types.Labels) (*upack, error) {
 	auth := s.server.Auth()
 	upriv, upub, err := auth.GenerateKeyPair("")
 	if err != nil {
